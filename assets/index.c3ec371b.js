c7a38dde490947a38421ee4c1100198608511e63
sagun-k <sagunkshrestha7@gmail.com>
2021-07-25 13:54:46 +0545

    commit

---
+++ assets/index.c3ec371b.js
@@ -0,0 +1,23 @@
+
+
+function displaymenu(){
+    
+    document.getElementById("menu").style.display="block";
+    document.getElementById("hamburger").style.display="hidden";
+    document.getElementById("cross").style.display="block";
+    console.log("it worked");
+
+     }
+     
+function closemenu(){
+    console.log("It worked!!!");
+    document.getElementById("menu").style.display="none";
+    document.getElementById("hamburger").style.display="block";
+    document.getElementById("cross").style.display="none";
+}
+     
+  
+
+ 
+
+
